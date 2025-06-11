3d07b6c906d1e1a4a61d704d8de77eac1b0e658a
Tobias Aguiar <tobias.aguiarsilvadealmeida@nxp.com>
2025-06-11 17:58:49 +0200

    freertos: SDK 3.0: add mcux sdk_next extensions to rtos-abstraction-layer component
    Add support for MCUX SDK 3.0 extensions for freertos within
    rtos-abstraction-layer component.
    
    Signed-off-by: Tobias Aguiar <tobias.aguiarsilvadealmeida@nxp.com>

---
+++ freertos/mcux_rtos_abstraction_layer.cmake
@@ -0,0 +1,20 @@
+message("rtos_abstraction_layer (freertos) headers are included for target ${RTOS_ABSTRACTION_LAYER_TARGET}")
+
+mcux_add_include(
+    INCLUDES
+    include
+)
+
+include_guard(GLOBAL)
+message("rtos_abstraction_layer (freertos) sources are included for target ${RTOS_ABSTRACTION_LAYER_TARGET}")
+
+mcux_add_source(
+    SOURCES
+    rtos_atomic.c
+    rtos_mqueue.c
+    rtos_sched.c
+    rtos_stdio.c
+    rtos_timer.c
+    rtos_thread.c
+    include/*.h
+)
